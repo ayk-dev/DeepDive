776dc7a378ea688567bc2354da50a6b4f06dd961
Albena Krasteva <albena.y.krasteva@gmail.com>
2026-02-07 21:42:49 +0200

    some demo code

---
+++ demo_file.py
@@ -0,0 +1,23 @@
+a = [1, 2, 3]
+
+b = [1, 2,
+     3, 4]
+
+c = [
+    1, # comment
+    2, # comment
+    3
+]
+
+my_dict = {
+    'key1': 1, # comment
+    'key2': 2 # comment
+}
+
+print(my_dict)
+
+my_str = '''this is
+a string'''
+
+print(my_str)
+
